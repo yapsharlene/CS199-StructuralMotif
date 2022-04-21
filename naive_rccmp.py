68b8a4eb2ffb3dc8d485aefae50a702d5573f732
yapsharlene <71448190+yapsharlene@users.noreply.github.com>
2022-04-21 18:06:30 +0800

    (4/21/2022) updated naive_rccmp.py

--- naive_rccmp.py
+++ naive_rccmp.py
@@ -1,21 +1,38 @@
 from pdb_input import *
 
-# === NAIVE RCCMP === #
 # INPUT:
-# - n protein structures (P_1, P_2, ..., P_n)
+# - n protein structs (P_1, P_2, ..., P_n)
 # - length of RCCM l
 # - C number of residues
 # - r samples
 
-
 # === MAIN === #
 
-structs = get_structures("PDB files\Ref2")
-for struct in structs:
-    grab_ca_coords(struct)
+BENCHMARK_LENGTH = 3
 
+# Get all protein structs
+structs = get_structures("PDB files\Ref1")
 
+# (1) Fix P_1, translate other proteins to make centroids coincide
+fixed_struct = structs[0]
 
+superimposer = Bio.PDB.Superimposer()
+for i in range(1, len(structs)):
+    # print_ca_coords(get_ca_atoms(structs[i], 3))
+    superimposer.set_atoms(get_ca_atoms_of_range(fixed_struct, BENCHMARK_LENGTH), get_ca_atoms_of_range(structs[i], BENCHMARK_LENGTH))
+    superimposer.apply(get_ca_atoms_of_range(structs[i], BENCHMARK_LENGTH))
+    # print(superimposer.rms)
+    # print(superimposer.rotran)
+    print("Superimposed alpha carbons of {0} to be as close as possible to {1}".format(structs[i].id, fixed_struct.id))
 
+# (2) Select a length-l compact motif u1, u2, …, ur
+# where ui is a motif of some Pj and x is the total number of samples
+        
 
+# (3) Select a transformation 𝜏2𝜏3,...,𝜏r
 
+# (3a) Find the median for each discrete rigid transformation u
+
+# (3b) Find the compact motif that minimizes the cMSD distance vi where i = 1, 2, … n
+
+# (3c) Compute the objective function value c(u)
--- pdb_input.py
+++ pdb_input.py
@@ -1,3 +1,4 @@
+import Bio.PDB
 import os
 import warnings
 from Bio.PDB.PDBParser import PDBParser
@@ -23,21 +24,17 @@ def get_structures(folder):
 
     return pdb_structs
 
-# Given a structure, print out the xyz-coordinates
+# Given a list of atoms, print out the xyz-coordinates
 # of each carbon atom
-def print_ca_coords(struct):
+def print_ca_coords(ca_list):
     print('{:10}|{:10}|{:10}'.format("X","Y","Z"))
-    for model in struct:
-        for chain in model:
-            for residue in chain:
-                for atom in residue:
-                    if atom.get_id() == "CA":
-                        x,y,z = atom.get_coord()
-                        print('{:<10.3f}|{:<10.3f}|{:<10.3f}'.format(x, y, z))
+    for atom in ca_list:
+        x,y,z = atom.get_coord()
+        print('{:<10.3f}|{:<10.3f}|{:<10.3f}'.format(x, y, z))
                         
 # Given a structure, return a list of xyz-coordinates
 # of each carbon atom
-def grab_ca_coords(struct):
+def get_ca_coords(struct):
     atoms = []
     for model in struct:
         for chain in model:
@@ -47,3 +44,41 @@ def grab_ca_coords(struct):
                         atoms.append(atom.get_coord())
     print("Grabbed {0} coordinates from {1}".format(len(atoms), struct.id))
     return atoms
+
+def get_ca_atoms(struct):
+    atoms = []
+    for model in struct:
+        for chain in model:
+            for residue in chain:
+                for atom in residue:
+                    if atom.get_id() == "CA":
+                        atoms.append(atom)
+    return atoms
+
+# Given a structure, return a list of Atoms with ID alpha carbon
+def get_ca_atoms_of_range(range, struct):
+    atoms = []
+    r = 0
+    for model in struct:
+        for chain in model:
+            for residue in chain:
+                for atom in residue:
+                    if r >= range:
+                        break
+                    if atom.get_id() == "CA":
+                        atoms.append(atom)
+                        r += 1
+    return atoms
+
+# Return the centroid of a given atom list
+def get_centroid(atom_list):
+    centroid = [0, 0, 0]
+    for atom in atom_list:
+        centroid[0] += atom[0]
+        centroid[1] += atom[1]
+        centroid[2] += atom[2]
+
+    for i in range(3):
+        centroid[i] = centroid[i]/len(atom_list)
+
+    return (centroid[0], centroid[1], centroid[2])
